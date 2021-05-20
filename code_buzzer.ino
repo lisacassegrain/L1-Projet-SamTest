3f50668b37e084e926c02b2315403116d5354073
lisacassegrain <84515836+lisacassegrain@users.noreply.github.com>
2021-05-20 16:27:47 +0200

    Add files via upload

---
+++ code_buzzer.ino
@@ -0,0 +1,47 @@
+int buzzer =2;// Borne de sortie
+
+void setup()
+
+{
+
+pinMode(buzzer,OUTPUT);// Définir la sortie du buzzer
+
+}
+
+void loop()
+
+{
+
+int i;// Définir 1 variable pour faire des boucles
+
+
+
+
+
+for(i=0; i<80; i++)// Premier son à une fréquence
+
+{
+
+digitalWrite(buzzer, HIGH);// Faire du bruit
+
+delay(1000);// Attendre 10ms
+
+digitalWrite(buzzer, LOW);// Silencendelay(10);// Attendre 10ms
+
+delay(1000);// Attendre 10ms
+
+}
+
+for(i=0; i<40; i++)// Deuxième son à une autre fréquence
+
+{
+
+delay(20);// Attendre 20msndigitalWrite(buzzer,LOW);// Silence
+
+delay(20);// Attendre 20msn}
+
+}
+
+
+
+}
